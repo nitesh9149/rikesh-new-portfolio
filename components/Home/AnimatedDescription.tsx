38159219eb8cfccf1fc0ef47151dfcdca2fbf0ab
Nitesh <nitesh.rajbanshi@gmail.com>
2025-05-12 17:02:54 +0545

    new changes

--- components/Home/AnimatedDescription.tsx
+++ components/Home/AnimatedDescription.tsx
@@ -4,9 +4,9 @@ import { motion } from "framer-motion"
 
 export default function AnimatedDescription() {
   return (
-    <h3 className="flex animate-fade-up-5 flex-col text-center text-lg opacity-0">
+    <h3 className="flex animate-fade-up-5 flex-col text-center text-xl font-semibold opacity-0">
       Over the last 5y, I&apos;ve empowered +5 companies
-      <span className="max-w-[42rem] animate-fade-up-6 leading-normal text-muted-foreground opacity-0 sm:text-xl sm:leading-8">
+      <span className="max-w-[42rem] animate-fade-up-6 pt-2 text-lg font-light leading-normal opacity-0 sm:leading-8">
         Designing user-centric experiences that captivate, engage, and create
         meaningful impact.
       </span>
--- components/Home/HeaderImage.tsx
+++ components/Home/HeaderImage.tsx
@@ -3,17 +3,17 @@ import Image from "next/image"
 export default function HeaderImage() {
   return (
     <div className="animate-fade-in opacity-0">
-      <div className="absolute inset-x-0 top-0 z-10 h-[50vh] w-full bg-gradient-to-b from-[hsl(244,31%,10%)] to-transparent" />
+      <div className="absolute inset-x-0 top-0 z-10 h-[50vh] w-full bg-gradient-to-b from-[rgba(19,18,33,0.2)] to-transparent" />
       <div className="absolute inset-x-0 bottom-0 z-10 h-[50vh] w-full bg-gradient-to-t from-[hsl(244,31%,10%)] to-transparent" />
 
       <Image
-        src="/images/home/bg-header.jpg"
+        src="/images/home/bg-header.png"
         width={720}
         height={720}
         placeholder="blur"
         blurDataURL="/images/home/bg-header-blur.jpg"
         alt="Background - Rikesh Niroula Profile"
-        className="absolute inset-0 h-[100dvh] w-full object-cover opacity-25 transition-all duration-1000"
+        className="absolute inset-0 h-[100dvh] w-full object-cover opacity-50 transition-all duration-1000"
         priority
       />
     </div>
